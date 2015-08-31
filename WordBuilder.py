f32ecac338955ea0a80cac0851f9328703025670
Mouse Reeve <mouse.reeve@gmail.com>
2015-08-31 14:35:25 -0700

    Use average word length
    Also, assume one word per line

--- WordBuilder.py
+++ WordBuilder.py
@@ -1,4 +1,4 @@
-''' Create new words! '''
+''' Create words from an existing wordlist '''
 import random
 import re
 
@@ -16,14 +16,23 @@ class WordBuilder(object):
             self.initial: []
         }
 
+        self.average_word_length = 0
+        self.shortest = None
+
 
     def ingest(self, corpus_file):
         ''' load and parse a pre-formatted and cleaned text file. Garbage in, garbage out '''
         corpus = open(corpus_file)
-        for word in corpus.read().split(' '):
+        total_letters = 0
+        total_words = 0
+        shortest_word = 100
+        for word in corpus.readlines():
             # clean word
             word = word.strip()
             word = re.sub(r'[\',\.\"]', '', word)
+            total_letters += len(word)
+            total_words += 1
+            shortest_word = len(word) if len(word) < shortest_word else shortest_word
 
             # iterate through n letter groups, where 1 <= n <= 3
             n = self.chunk_size
@@ -37,21 +46,26 @@ class WordBuilder(object):
                     self.links[base] = []
 
                 self.links[base].append(word[position])
-            if not word[n:len(word)] in self.links:
-                self.links[word[n:len(word)]] = []
-            self.links[word[n:len(word)]].append(self.terminal)
+            if not word[-n:] in self.links:
+                self.links[word[-n:]] = []
+            self.links[word[-n:]].append(self.terminal)
+
+        self.average_word_length = total_letters / total_words
+        self.shortest = shortest_word
 
 
     def get_word(self, word=None):
         ''' creates a new word '''
         word = word if not word == None else self.initial
         if not self.terminal in word:
-            word += random.choice(self.links[word[-self.chunk_size:]])
+            if len(word) > self.average_word_length and \
+                    self.terminal in self.links[word[-self.chunk_size:]] \
+                    and random.randint(0, 1):
+                addon = self.terminal
+            else:
+                options = self.links[word[-self.chunk_size:]]
+                addon = random.choice(options)
+
+            word = word + addon
             return self.get_word(word)
-        return word
-
-
-if __name__ == '__main__':
-    builder = WordBuilder()
-    builder.ingest('science.txt')
-    print builder.get_word()
+        return word[1:-1]
