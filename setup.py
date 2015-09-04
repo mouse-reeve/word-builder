e83e82ad294dff15ca1ae212d5508c55b44ea17c
Mouse Reeve <mouse.reeve@gmail.com>
2015-09-04 15:07:25 -0700

    Improves packaging options

--- setup.py
+++ setup.py
@@ -1,16 +1,39 @@
 ''' setup script '''
-from distutils.core import setup
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='WordBuilder',
-    version='0.1.1',
+    version='0.1.2',
+
+    description='Creates new words based on an input corpus',
+    long_description=long_description,
+
+    url='https://github.com/mouse-reeve/word-builder',
+
     author='Mouse Reeve',
     author_email='mouse.reeve@gmail.com',
 
-    packages=['app'],
+    license='MIT',
 
-    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
 
-    url='https://github.com/mouse-reeve/word-builder',
+        'Intended Audience :: Developers',
+        'Topic :: Artistic Software',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7'
+    ],
+
+    keywords='generative language',
+
+    packages=['word-builder'],
 )
 
